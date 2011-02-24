96f5ee22dfef568e6910953bf40fe34fe929d692
Chenoa <cowmanifestation@gmail.com>
2011-02-23 19:10:58 -0500

    Still needs some refactoring.  Is it bad to make lots of instance variables so i don't have to pass so many arguments all the time?

--- mad_libs.rb
+++ mad_libs.rb
@@ -5,39 +5,40 @@ class MadLibs
     run
   end
 
-  def get_substitutions(prompts)
+  def get_substitutions(sentence, prompts)
     prompts.each do |prompt|
       if prompt[0]
         @keywords << prompt[0]
         print prompt[1] + ": "
         substitution = gets.chomp
         [prompt[0] + ":" + prompt[1], prompt[0]].each do |p|
-          substitute(p, substitution)
+          substitute(sentence, p, substitution)
         end
       else
         unless @keywords.include?(prompt[1])
           print prompt[1] + ": "
           substitution = gets.chomp
-          substitute(prompt[1], substitution)
+          substitute(sentence, prompt[1], substitution)
         end
       end
     end
   end
 
-  def substitute(pattern, substitution)
-    @sentence.gsub!(/\(\(#{pattern}\)\)/, substitution)
+  def substitute(sentence, pattern, substitution)
+    sentence.gsub!(/\(\(#{pattern}\)\)/, substitution)
   end
 
   def run
     File.open(@file) do |file|
       file.each do |line|
-        @sentence = line
-        prompts = @sentence.scan(/\(\((?:(\w+):)?([\w\s]+)\)\)/)
+        sentence = line
+        prompts = sentence.scan(/\(\((?:(\w+):)?([\w\s]+)\)\)/)
         @keywords = []
 
-        get_substitutions(prompts)
+        get_substitutions(sentence, prompts)
 
-        puts @sentence
+        puts sentence
+
         print "Continue? (y/n)"
         answer = gets
         if answer == "n\n"
