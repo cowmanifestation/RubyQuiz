cc19eb557f987cb62b81205e316cbd80a60cdc88
Chenoa <cowmanifestation@gmail.com>
2011-02-24 20:01:51 -0500

    Now refactored.

--- mad_libs.rb
+++ mad_libs.rb
@@ -5,44 +5,58 @@ class MadLibs
     run
   end
 
-  def get_substitutions(sentence, prompts)
+  def get_prompts(sentence)
+    sentence.scan(/\(\((?:(\w+):)?([\w\s]+)\)\)/)
+  end
+
+  def get_substitution(prompt)
+    print prompt + ": "
+    substitution = gets.chomp
+    return substitution
+  end
+
+  def substitute(sentence, keywords, prompts)
     prompts.each do |prompt|
       if prompt[0]
-        @keywords << prompt[0]
-        print prompt[1] + ": "
-        substitution = gets.chomp
+        keywords << prompt[0]
+        substitution = get_substitution(prompt[1])
         [prompt[0] + ":" + prompt[1], prompt[0]].each do |p|
-          substitute(sentence, p, substitution)
+          replace(sentence, p, substitution)
         end
       else
-        unless @keywords.include?(prompt[1])
-          print prompt[1] + ": "
-          substitution = gets.chomp
-          substitute(sentence, prompt[1], substitution)
+        unless keywords.include?(prompt[1])
+          substitution = get_substitution(prompt[1])
+          replace(sentence, prompt[1], substitution)
         end
       end
     end
   end
 
-  def substitute(sentence, pattern, substitution)
+  def replace(sentence, pattern, substitution)
     sentence.gsub!(/\(\(#{pattern}\)\)/, substitution)
   end
+
+  def ask_if_continuing(file)
+    print "Continue? (y/n) "
+    answer = gets
+    if answer == "n\n"
+      exit
+    end
+  end
+
 
   def run
     File.open(@file) do |file|
       file.each do |line|
-        sentence = line
-        prompts = sentence.scan(/\(\((?:(\w+):)?([\w\s]+)\)\)/)
-        @keywords = []
+        keywords = []
+        prompts = get_prompts(line)
 
-        get_substitutions(sentence, prompts)
+        substitute(line, keywords, prompts)
 
-        puts sentence
+        puts line
 
-        print "Continue? (y/n)"
-        answer = gets
-        if answer == "n\n"
-          exit
+        unless file.eof?
+          ask_if_continuing(file)
         end
       end
     end
